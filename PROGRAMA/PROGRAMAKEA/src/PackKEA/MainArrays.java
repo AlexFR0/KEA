67d2bcea1f895a86e4bc525b8a7f7e72863132d0
AlexFR0 <afrancesretamero50@gmail.com>
2024-11-18 19:29:28 +0100

    SumArrays

--- PROGRAMA/PROGRAMAKEA/src/PackKEA/MainArrays.java
+++ PROGRAMA/PROGRAMAKEA/src/PackKEA/MainArrays.java
@@ -5,20 +5,18 @@ public class MainArrays {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] sumArray = {4,36,4,23,867};
-		int[] subArray = {785675, 56, 5675, 56544, 234};
-		int[] prodArray = {6, 8, 20, 23, 12};
-		
-		// User 1 fer la suma dels elements de sumArray
-		
-		
-		
-		// User 2 fer la resta dels elements de subArray
-		
-		
-		
-		// User 3 fer la multiplicació dels elements de prodArray
-	}
-	
-	}
+		// Declaración del arreglo
+        int[] sumArray = {4, 36, 4, 23, 867};
 
+        // Variable para almacenar la suma
+        int suma = 0;
+
+        // Bucle para recorrer el arreglo y sumar los elementos
+        for (int num : sumArray) {
+            suma += num;
+        }
+
+        // Mostrar el resultado
+        System.out.println("La suma de los elementos del arreglo es: " + suma);
+    }
+}
